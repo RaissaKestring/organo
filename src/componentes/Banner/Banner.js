c95431fa32a4fc81fce2b038ea1d3abbc81209dc
RaissaKestring <raissakestring23@gmail.com>
2024-04-17 16:06:56 -0300

    Depurando código

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,16 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 export const Banner = () => {
     // JSX (como o react le isso tudo e transforma em DOM)
     // importar classe em React é className(importo a classe do css)
+    // Fragment = <> e </>
     return (
-        <header className="banner"> 
-        <img src="/imagens/banner.png" alt="O banner principal da página Organo"/>
-        </header>
+        <> 
+            <header className="banner"> 
+            <img src="/imagens/banner.png" alt="O banner principal da página Organo"/>
+            </header>
+            <h1>Teste</h1>
+        </>
     )
 }
